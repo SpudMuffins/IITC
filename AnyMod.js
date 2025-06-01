c36dbce7a5a9bdcac01601c77acb89d1c8a30f6e
SpudMuffins <mateo@chavezm.com>
2025-06-01 13:46:02 -0700

    Update AnyMod.js

--- AnyMod.js
+++ AnyMod.js
@@ -1,46 +1,46 @@
 // ==UserScript==
-// @id             highlight-any-mod@spudmuffins
-// @name           Highlight: Any Mods Present
-// @category       Highlighter
-// @version        0.1.0
+// @id             mod-finder-layer@spudmuffins
+// @name           Modded Portals Layer
+// @category       Layer
+// @version        1.0.0
 // @namespace      https://github.com/SpudMuffins/IITC
-// @description    Highlights portals that have any mod installed.
+// @description    Highlights portals that have any mod installed
+// @include        https://intel.ingress.com/*
 // @match          https://intel.ingress.com/*
 // @grant          none
 // ==/UserScript==
 
 function wrapper(plugin_info) {
-  if (typeof window.plugin !== 'function') window.plugin = () => {};
-  window.plugin.anyModHighlighter = {};
+  if (typeof window.plugin !== 'function') window.plugin = function () {};
 
-  window.plugin.anyModHighlighter.highlight = function (data) {
-    const mods = data.portal.options.data?.mods;
-    if (mods && mods.some(m => m)) {
-      data.portal.setStyle({
-        fillColor: '#00ffff',
-        fillOpacity: 0.7,
-        strokeColor: '#000',
-        strokeOpacity: 0.5,
-        strokeWeight: 2,
+  window.plugin.moddedPortals = {
+    layerGroup: null,
+
+    setup: function () {
+      this.layerGroup = new L.LayerGroup();
+      window.addLayerGroup('Modded Portals', this.layerGroup, true);
+
+      // Hook into portalAdded
+      window.addHook('portalAdded', function (obj) {
+        const data = obj.portal.options.data;
+        if (!data || !Array.isArray(data.mods)) return;
+
+        const hasMod = data.mods.some(mod => mod !== null);
+        if (hasMod) {
+          const circle = L.circle(obj.portal.getLatLng(), {
+            radius: 20,
+            color: 'orange',
+            fillColor: 'orange',
+            fillOpacity: 0.5,
+            weight: 2,
+            interactive: false,
+          });
+          circle.addTo(window.plugin.moddedPortals.layerGroup);
+        }
       });
-    } else {
-      data.portal.setStyle({
-        fillOpacity: 0.1,
-        strokeOpacity: 0.1,
-      });
-    }
-  };
 
-  const setup = function () {
-    window.addPortalHighlighter('Mods: Any Present', window.plugin.anyModHighlighter.highlight);
-  };
+      // Recheck all portals when toggled
+      window.addHook('mapDataRefreshEnd', this.refreshAll);
+    },
 
-  setup.info = plugin_info;
-  if (!window.bootPlugins) window.bootPlugins = [];
-  window.bootPlugins.push(setup);
-  if (window.iitcLoaded) setup();
-}
-
-const script = document.createElement('script');
-script.appendChild(document.createTextNode('(' + wrapper + ')({});'));
-(document.body || document.head || document.documentElement).appendChild(script);
+    refreshAll: functi
