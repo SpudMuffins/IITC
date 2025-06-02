31e095336575ff47aa889be94f2c539236f32fc8
SpudMuffins <mateo@chavezm.com>
2025-06-01 20:38:30 -0700

    Update AnyModsOverlay.js

--- AnyModsOverlay.js
+++ AnyModsOverlay.js
@@ -1,71 +1,85 @@
 // ==UserScript==
-// @id             overlay-mods-installed
-// @name           Overlay: Portals with Mods
+// @id             iitc-plugin-overlay-mods-installed
+// @name           IITC plugin: Overlay - Portals with Mods
 // @category       Layer
-// @version        0.2.1
-// @description    [overlay v0.2.1] Highlight portals that have one or more mods installed.
+// @version        0.2.0
+// @description    [overlay v0.2.0] Adds an overlay that marks portals with one or more mods installed.
 // @include        https://intel.ingress.com/intel*
 // @match          https://intel.ingress.com/intel*
 // @grant          none
 // ==/UserScript==
 
-// Define the plugin
-window.plugin = window.plugin || {};
-window.plugin.modsOverlay = {};
+function wrapper(plugin_info) {
+  if (typeof window.plugin !== 'object') window.plugin = {};
+  window.plugin.modsOverlay = function () {};
 
-window.plugin.modsOverlay.layer = new L.LayerGroup();
-window.plugin.modsOverlay.portalGuidsSeen = new Set();
-window.plugin.modsOverlay.MIN_ZOOM = 15;
+  const self = window.plugin.modsOverlay;
 
-// Add the layer to IITC
-window.addLayerGroup('Portals with Mods', window.plugin.modsOverlay.layer, true);
+  self.layerGroup = new L.LayerGroup();
+  self.layerName = 'Portals with Mods';
 
-// Add logic: when a portal detail is loaded
-window.addHook('portalDetailLoaded', function (data) {
-  const mods = data.details.mods;
-  const guid = data.guid;
+  self.setup = function () {
+    window.addLayerGroup(self.layerName, self.layerGroup, true);
+    window.addHook('portalDetailLoaded', self.onPortalDetailLoaded);
+    window.map.on('moveend', self.checkVisiblePortals);
+    self.checkVisiblePortals();
+  };
 
-  if (!mods || !mods.some(mod => mod)) return;
-  if (window.plugin.modsOverlay.portalGuidsSeen.has(guid)) return;
+  self.onPortalDetailLoaded = function (data) {
+    const portal = window.portals[data.guid];
+    const mods = data.details.mods;
 
-  const portal = window.portals[guid];
-  if (!portal) return;
+    if (portal && mods && mods.some(m => m)) {
+      const latlng = portal.getLatLng();
 
-  const latlng = portal.getLatLng();
-  const circle = L.circleMarker(latlng, {
-    radius: 18,
-    color: '#ff00ff',
-    weight: 3,
-    opacity: 0.7,
-    fillOpacity: 0,
-    interactive: false
-  });
+      const circle = L.circleMarker(latlng, {
+        radius: 18,
+        color: '#ff00ff',
+        weight: 3,
+        opacity: 0.7,
+        fillOpacity: 0,
+        interactive: false
+      });
 
-  window.plugin.modsOverlay.layer.addLayer(circle);
-  window.plugin.modsOverlay.portalGuidsSeen.add(guid);
-});
-
-// Add logic: when map finishes loading data
-window.addHook('mapDataRefreshEnd', function () {
-  const zoom = window.map.getZoom();
-  if (zoom < window.plugin.modsOverlay.MIN_ZOOM) {
-    console.warn('[Mods Overlay] Too zoomed out to detect mods');
-    window.plugin.modsOverlay.layer.clearLayers();
-    window.plugin.modsOverlay.portalGuidsSeen.clear();
-    return;
-  }
-
-  window.plugin.modsOverlay.layer.clearLayers();
-  window.plugin.modsOverlay.portalGuidsSeen.clear();
-
-  const bounds = window.map.getBounds();
-  for (const guid in window.portals) {
-    const portal = window.portals[guid];
-    if (!portal) continue;
-
-    const latlng = portal.getLatLng();
-    if (bounds.contains(latlng)) {
-      window.portalDetail.request(guid);
+      self.layerGroup.addLayer(circle);
     }
+  };
+
+  self.checkVisiblePortals = function () {
+    const zoom = window.map.getZoom();
+    if (zoom < 15) {
+      return;
+    }
+
+    self.layerGroup.clearLayers();
+
+    const bounds = window.map.getBounds();
+
+    for (const guid in window.portals) {
+      const portal = window.portals[guid];
+      if (!portal) continue;
+
+      const latlng = portal.getLatLng();
+      if (bounds.contains(latlng)) {
+        window.portalDetail.request(guid);
+      }
+    }
+  };
+
+  const setup = self.setup;
+  setup.info = plugin_info;
+  if (window.iitcLoaded) {
+    setup();
+  } else {
+    window.bootPlugins = window.bootPlugins || [];
+    window.bootPlugins.push(setup);
   }
-});
+}
+
+const script = document.createElement('script');
+script.appendChild(document.createTextNode('(' + wrapper + ')(' + JSON.stringify({
+  buildName: 'overlay-mods-installed',
+  dateTimeVersion: '2025-06-01',
+  pluginId: 'overlay-mods-installed'
+}) + ');'));
+(document.body || document.head || document.documentElement).appendChild(script);
