b2e1298478b74ea40379959b739644096b007c71
SpudMuffins <mateo@chavezm.com>
2025-06-01 17:46:18 -0700

    Update AnyMod.js

--- AnyMod.js
+++ AnyMod.js
@@ -2,7 +2,7 @@
 // @id             highlight-portals-with-mods
 // @name           IITC plugin: Highlight Portals with Mods
 // @category       Highlighter
-// @version        0.1
+// @version        0.2
 // @description    Highlight portals that have at least one mod installed.
 // @include        https://intel.ingress.com/intel*
 // @match          https://intel.ingress.com/intel*
@@ -11,16 +11,25 @@
 
 function setup() {
   window.addPortalHighlighter('Portals with Mods', function(data) {
-    const mods = data.portal.options.data.mods;
-    if (mods && mods.some(mod => mod !== null)) {
-      // Set fillColor to yellow for visibility
-      data.portal.setStyle({ fillColor: 'yellow', fillOpacity: 0.6 });
+    const portalData = data.portal.options.data;
+
+    if (!portalData || !Array.isArray(portalData.mods)) return;
+
+    const hasMods = portalData.mods.some(mod => mod !== null);
+    console.log('Highlighting portal:', data.portal.options.guid, 'Has mods?', hasMods);
+
+    if (hasMods) {
+      data.portal.setStyle({
+        fillColor: 'yellow',
+        fillOpacity: 0.6,
+        color: '#FFFF00'
+      });
     }
   });
 }
 
 setup.info = {
-  version: '0.1',
+  version: '0.2',
   description: 'Highlight portals that have mods installed',
   category: 'Highlighter',
 };
